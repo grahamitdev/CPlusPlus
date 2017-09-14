3c91fe1a15df6e3aef0e1b487dd1f9efd3a15918
walter <gzyiot2017@163.com>
2017-09-14 16:35:05 +0800

    add cmp.cpp complen.cpp printfstr.cpp

---
+++ one/string/cmp.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(void)
+{
+	string str1;
+	string str2;
+	cout << "请输入:";
+	cin >> str1;
+	cout << "请输入:";
+	cin >> str2;
+	if(str1 > str2)
+	{
+		cout << str1 << endl;
+	}
+	else if(str1 < str2)
+	{
+		cout << str2 << endl;
+	}
+	else
+	{
+		cout << str1 << "=" << str2 << endl;
+	}
+	
+
+
+	return 0;
+}
